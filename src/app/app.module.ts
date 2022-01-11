f55560f8868c7c5f865e76199a2a4a29d9c5cc67
Luiz Vieira <luiz22_sp@hotmail.com>
2022-01-10 21:40:15 -0300

    02 autenticação e o papel do token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
+import { CoreModule } from './core/core.module';
 import { ErrorsModule } from './errors/errors.module';
 import { HomeModule } from './home/home.module';
 import { PhotosModule } from './photos/photos.module';
@@ -16,7 +17,8 @@ import { PhotosModule } from './photos/photos.module';
     AppRoutingModule,
     PhotosModule,
     ErrorsModule,
-    HomeModule
+    HomeModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 const HTTP_HOST = 'http://localhost:3000';
 
@@ -9,10 +11,18 @@ const HTTP_HOST = 'http://localhost:3000';
 export class AuthService {
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private tokenService: TokenService
   ) { }
 
   autentications(userName: string, password: string) {
-    return this.http.post(`${HTTP_HOST}/user/login`, {userName, password});
+    return this.http.post(`${HTTP_HOST}/user/login`, {userName, password}, {observe: 'response'})
+    .pipe(
+      tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
+        console.log(`User ${userName} authenticated with token ${authToken}`);
+      })
+    )
   }
 }
